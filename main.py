f3841458e3748a9db9f7b7d6ae2083811da4797d
vitas <pirozhkov.vitaly@gmail.com>
2022-03-29 09:23:48 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import logging
+import AI
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Отправь фото')
+
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def gender_check(update: Update, context: CallbackContext) -> None:
+    file_id = update.message.photo[-1].file_id
+    new_file = context.bot.get_file(file_id)
+    new_file.download('test.jpg')
+    photo = './test.jpg'
+    genders = AI.resolve(photo)
+    if genders:
+        for gender in genders:
+            update.message.reply_text(gender)
+    else:
+        update.message.reply_text("Не вижу людей")
+
+
+def main() -> None:
+    updater = Updater("5142388894:AAGopokQ8IsAkhbOSs2HhPGPMWa8KFUM3_0")
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.photo, gender_check))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
